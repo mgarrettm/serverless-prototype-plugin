0f957a50287352122fec7d54e091f10b2df41fae
Garrett McGrath <Garrett McGrath>
2017-02-19 14:00:37 -0800

    fix serverless reference

--- deploy/lib/createFunctions.js
+++ deploy/lib/createFunctions.js
@@ -7,7 +7,7 @@ module.exports = {
     const createFunctionPromises = [];
 
     this.serverless.service.getAllFunctions().forEach((functionName) => {
-      const functionObject = serverless.service.getFunction(functionName);
+      const functionObject = this.serverless.service.getFunction(functionName);
 
       createFunctionPromises.push(this.provider.generateZipString(functionName, functionObject.handler)
         .then(zipString => this.provider.uploadFunction(functionName, zipString, functionObject)));
